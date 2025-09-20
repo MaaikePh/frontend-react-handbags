3671ac8fe23983fe39d029a5c766602a2553a033
MaaikePh <maaikephielix@live.nl>
2025-09-20 13:01:12 +0200

    Product component gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import './App.css'
-import Button from "./components/Button.jsx";
+import './App.css';
+import Button from './components/Button.jsx';
+import bag1 from './assets/bag_1.png';
+import bag2 from './assets/bag_2.png';
+import bag3 from './assets/bag_3.png';
+import bag4 from './assets/bag_4.png';
+import Product from './components/Product.jsx';
 
 function App() {
 
@@ -11,6 +16,12 @@ function App() {
                 <Button label={'shop all bags'} disabled={false} />
                 <Button label={'pre-orders'} disabled={true} />
             </nav>
+            <main>
+                <Product productImage={bag1} imageAltText={'The handy bag'} productTitle={'The handy bag'} productPrice={'€400,-'} isBestSeller={true} isNewCollection={false}/>
+                <Product productImage={bag2} imageAltText={'The stylish bag'} productTitle={'The stylish bag'} productPrice={'€250,-'} isBestSeller={true} isNewCollection={false}/>
+                <Product productImage={bag3} imageAltText={'The simple bag'} productTitle={'The simple bag'} productPrice={'€300,-'} isBestSeller={false} isNewCollection={true}/>
+                <Product productImage={bag4} imageAltText={'The trendy bag'} productTitle={'The trendy bag'} productPrice={'€150,-'} isBestSeller={false} isNewCollection={true}/>
+            </main>
         </>
     )
 }
